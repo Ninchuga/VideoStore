743332c31b4fa11fc13a8b5361679edbdfc2fafb
Nino <ninoslav90@hotmail.com>
2023-10-30 22:30:14 +0100

    BaseResult model added to VideoStore.Shared, and MoviesController refactored for exception handling.

--- src/services/movies/VideoStore.Movies/Controllers/MoviesController.cs
+++ src/services/movies/VideoStore.Movies/Controllers/MoviesController.cs
@@ -7,6 +7,7 @@ using VideoStore.Movies.Constants;
 using VideoStore.Movies.DTOs;
 using VideoStore.Movies.Extensions;
 using VideoStore.Movies.Infrastrucutre.Repositories;
+using VideoStore.Shared;
 using Movie = VideoStore.Movies.Models.Movie;
 
 namespace VideoStore.Movies.Controllers
@@ -18,13 +19,13 @@ namespace VideoStore.Movies.Controllers
     {
         private readonly IMovieRepository _movieRepository;
         private readonly ILogger<MoviesController> _logger;
-        private readonly IPublishEndpoint _publishEndpoint;
+        private readonly IServiceProvider _serviceProvider;
 
-        public MoviesController(IMovieRepository movieRepository, ILogger<MoviesController> logger, IPublishEndpoint publishEndpoint)
+        public MoviesController(IMovieRepository movieRepository, ILogger<MoviesController> logger, IServiceProvider serviceProvider)
         {
             _movieRepository = movieRepository;
             _logger = logger;
-            _publishEndpoint = publishEndpoint;
+            _serviceProvider = serviceProvider;
         }
 
         [HttpGet]
@@ -47,8 +48,21 @@ namespace VideoStore.Movies.Controllers
         [Route("addMovie")]
         public async Task<ActionResult<Movie>> AddMovie([FromBody] MovieDTO movie, CancellationToken cancellationToken)
         {
-            _movieRepository.UpsertMovie(movie.ToEntity());
-            await _movieRepository.SaveChanges(cancellationToken);
+            try
+            {
+                _movieRepository.UpsertMovie(movie.ToEntity());
+                await _movieRepository.SaveChanges(cancellationToken);
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                _logger.LogError(ex, "{ExceptionName} occurred while trying to add the movie movie with title {MovieTitle}.", nameof(DbUpdateConcurrencyException), movie.Title);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unhandled exception occurred while trying to add the movie with id {MovieTitle}.", movie.Title);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
 
             return Ok();
         }
@@ -66,9 +80,14 @@ namespace VideoStore.Movies.Controllers
             {
                 await _movieRepository.SaveChanges(cancellationToken);
             }
-            catch (DbUpdateConcurrencyException)
+            catch (DbUpdateConcurrencyException ex)
             {
-                _logger.LogError("{ExceptionName} occurred for movie with id {MovieId}.", nameof(DbUpdateConcurrencyException), id);
+                _logger.LogError(ex, "{ExceptionName} occurred while trying to update the movie movie with id {MovieId}.", nameof(DbUpdateConcurrencyException), id);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, "Unhandled exception occurred while trying to update the movie with id {MovieId}.", id);
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
 
@@ -83,51 +102,78 @@ namespace VideoStore.Movies.Controllers
             if (movie is null)
                 return NotFound();
 
-            _movieRepository.DeleteMovie(movie);
-            await _movieRepository.SaveChanges(cancellationToken);
+            try
+            {
+                _movieRepository.DeleteMovie(movie);
+                await _movieRepository.SaveChanges(cancellationToken);
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                _logger.LogError(ex, "{ExceptionName} occurred while trying to delete the movie with id {MovieId}.", nameof(DbUpdateConcurrencyException), id);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unhandled exception occurred while trying to delete the movie with id {MovieId}.", id);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
 
             return Ok();
         }
 
         [HttpPost]
         [Route("orderMovies")]
-        public async Task<IActionResult> BuyMovie([FromBody] IEnumerable<int> moviesId, CancellationToken cancellationToken)
+        public async Task<BaseResult> OrderMovies([FromBody] IEnumerable<int> moviesId, CancellationToken cancellationToken)
         {
+            var result = new BaseResult();
+
+            if (!moviesId.Any())
+                return result.AddError("At least one movie id must be specified.");
+
             string userEmail = User.Claims?.FirstOrDefault(c => c.Type.Equals(MoviesConstants.TokenClaimTypes.Email, StringComparison.OrdinalIgnoreCase))?.Value;
             if(string.IsNullOrWhiteSpace(userEmail))
-                return BadRequest("User email must have a value.");
+                return result.AddError("User email must have a value.");
 
             string userName = User.Claims?.FirstOrDefault(c => c.Type.Equals(MoviesConstants.TokenClaimTypes.Subject, StringComparison.OrdinalIgnoreCase))?.Value;
             if(string.IsNullOrWhiteSpace(userName))
-                return BadRequest("User name must have a value.");
+                return result.AddError("User name must have a value.");
 
             string userId = User.Claims?.FirstOrDefault(c => c.Type.Equals(MoviesConstants.TokenClaimTypes.UserId, StringComparison.OrdinalIgnoreCase))?.Value;
+            if (!int.TryParse(userId, out int parsedUserId))
+                return result.AddError($"User id {userId} is not a valid integer value.");
 
             var moviesToOrder = new List<Bus.Messages.Movie>();
             foreach (var movieId in moviesId)
             {
                 var movie = await _movieRepository.GetMovieBy(movieId, cancellationToken);
                 if (movie is null)
-                    return NotFound($"Movie with id: {movieId} was not found.");
+                {
+                    result.AddWarning($"Movie with id {movieId} not found.");
+                    continue;
+                }
 
                 moviesToOrder.Add(new Bus.Messages.Movie(movie.Id, movie.Title, movie.Price));
             }
 
-            if (!int.TryParse(userId, out int parsedUserId))
-                return BadRequest($"User id {userId} is not a valid integer value.");
+            if(moviesToOrder.Count == 0)
+                return result.AddWarning($"Movies with ids: '{string.Join(", ", moviesId)}' were not found.");
 
-            if (_publishEndpoint is null)
-                throw new Exception($"{nameof(IPublishEndpoint)} argument in {nameof(MoviesController)} cannot be null.");
+            var publishEndpoint = _serviceProvider.GetService<IPublishEndpoint>();
+            if (publishEndpoint is null)
+            {
+                _logger.LogError("{Controller}.{Action} service {ServiceName} cannot be resolved.", nameof(MoviesController), nameof(OrderMovies) , nameof(IPublishEndpoint));
+                return result.AddError($"Service '{nameof(IPublishEndpoint)}' could not be resolved.");
+            }
 
             var orderMovieMessage = new OrderMovieMessage(parsedUserId, userName, userEmail, moviesToOrder);
-            await _publishEndpoint.Publish(orderMovieMessage, context =>
+            await publishEndpoint.Publish(orderMovieMessage, context =>
             {
                 context.MessageId = Guid.NewGuid(); // new Guid("6ee234bb-c211-4756-bad1-c1c6e45c1b58");
                 context.CorrelationId = Guid.NewGuid();
                 context.TimeToLive = TimeSpan.FromMinutes(60); // if not consumed after this ttl, message will end up in dead-letter queue
             }, cancellationToken: cancellationToken);
 
-            return Ok();
+            return result;
         }
     }
 }
---
+++ src/shared/VideoStore.Shared/BaseResult.cs
@@ -0,0 +1,21 @@
+﻿namespace VideoStore.Shared
+{
+    public class BaseResult
+    {
+        public bool Success => Errors.Count == 0;
+        public List<string> Warnings { get; private set; } = new List<string>();
+        public List<string> Errors { get; private set; } = new List<string>();
+
+        public BaseResult AddError(string errorMessage)
+        {
+            Errors.Add(errorMessage);
+            return this;
+        }
+
+        public BaseResult AddWarning(string errorMessage)
+        {
+            Warnings.Add(errorMessage);
+            return this;
+        }
+    }
+}
